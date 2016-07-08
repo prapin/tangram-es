65f45d9e01f0ed18af270c6175422334485c5de7
Matt Blair <blair1618@gmail.com>
2016-07-08 13:33:22 -0400

    Remove scene file path argument from Map initializer

--- android/tangram/jni/jniExports.cpp
+++ android/tangram/jni/jniExports.cpp
@@ -102,7 +102,7 @@ extern "C" {
         setupJniEnv(jniEnv, tangramInstance, assetManager);
         const char* cStylePath = jniEnv->GetStringUTFChars(stylePath, NULL);
         assert(mapPtr > 0);
-        auto map = new Tangram::Map(cStylePath);
+        auto map = new Tangram::Map();
         jniEnv->ReleaseStringUTFChars(stylePath, cStylePath);
         return reinterpret_cast<jlong>(map);
     }
